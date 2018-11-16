658d02555076b58b4405492025820ff753278975
gubi <gubi.ale@iod.io>
2018-11-16 19:57:03 +0100

    Added Roles functions

--- classes/Roles.php
+++ classes/Roles.php
@@ -22,23 +22,42 @@
 class Roles extends Dataverse {
     /**
      * Create a New Role in a Dataverse
+     *
+     * Creates a new role in dataverse object whose Id is `dataverseIdtf` (that’s an id/alias)
+     * @see http://guides.dataverse.org/en/latest/api/native-api.html?highlight=import#id1
+     * @method POST
+     *
+     * @param string                            $dataverseIdtf                  A dataverse id (long) or a dataverse alias (more robust)
      */
-    public static function create_role() {
-
+    public static function create_role($dataverseIdtf) {
+        parent::check("\$dataverseIdtf", $dataverseIdtf);
+        parent::post("roles?dvo={$dataverseIdtf}");
     }
 
     /**
      * Show Role
+     *
+     * @see http://guides.dataverse.org/en/latest/api/native-api.html?highlight=import#show-role
+     * @method GET
+     *
+     * @param string                            $id                             The role ID
      */
-    public static function get_role() {
-
+    public static function get_role($id) {
+        parent::check("\$id", $id);
+        parent::get("roles/{$id}");
     }
 
     /**
      * Delete Role
+     *
+     * @see http://guides.dataverse.org/en/latest/api/native-api.html?highlight=import#delete-role
+     * @method DELETE
+     *
+     * @param string                            $id                             The role ID
      */
-    public static function delete_role() {
-
+    public static function delete_role($id) {
+        parent::check("\$id", $id);
+        parent::delete("roles/{$id}");
     }
 }
 ?>
