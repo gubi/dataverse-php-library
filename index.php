76d29918aaa81f926cd5c0c55615021fefed597a
Alessandro Gubitosi <gubi.ale@iod.io>
2018-11-18 12:30:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Content-type: application/json");
 require_once("Request_handler.php");
 
 Dataverse\Request_handler::set_api_key("<YOUR-API-KEY>");
-Dataverse\Request_handler::set_server("https://apitest.dataverse.org");
+Dataverse\Request_handler::set_server("https://dataverse.harvard.edu");
 $data = Metadata_blocks::get_all_metadata_info();
 
 print_r($data);
